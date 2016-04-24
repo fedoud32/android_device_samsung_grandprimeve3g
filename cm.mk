506c9d655af2c2425af46b2e6d972aaa4dde3ab8
Piyush Nagpal <piyushnagpal010@gmail.com>
2016-04-24 15:24:38 +0530

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := grandprimeve3g
+PRODUCT_RELEASE_NAME                        := grandprimeve3g
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -11,8 +11,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/samsung/grandprimeve3g/full_grandprimeve3g.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_MODEL := SM-G531H
-PRODUCT_BRAND := samsung
-PRODUCT_NAME := cm_grandprimeve3g
-PRODUCT_DEVICE := grandprimeve3g
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL                               := SM-G531H
+PRODUCT_BRAND                               := samsung
+PRODUCT_NAME                                := cm_grandprimeve3g
+PRODUCT_DEVICE                              := grandprimeve3g
+PRODUCT_MANUFACTURER                        := samsung
